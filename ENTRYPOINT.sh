156d8037ce5914c86a9c888ccbfe812fddc3d5bf
Kellegram <Kellegram@protonmail.ch>
2024-10-18 15:37:17 +0100

    Update README and ENTRYPOINT

--- ENTRYPOINT.sh
+++ ENTRYPOINT.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -eo pipefail
 if [ "$EULA" = "TRUE" ]; then
 	echo "eula=true" > eula.txt
 
@@ -12,7 +13,12 @@ if [ "$EULA" = "TRUE" ]; then
 	fi
 
 	grep "^sync-chunk-writes" server.properties || echo "sync-chunk-writes=false" >>server.properties
-	exec java -Xms6G -Xmx6G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=20 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar server.jar --nogui
+
+	if [ -n "$ALLOCATED_RAM" ]; then
+	exec java -Xms${ALLOCATED_RAM}G -Xmx${ALLOCATED_RAM}G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=20 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar server.jar --nogui
+	else
+		echo "Incorrect value for ALLOCATED_RAM provided or .env file missing. Please consult the README instructions if you haven't yet."
+	fi
 else
 	echo "Mojang EULA not accepted. Run with -e EULA=TRUE or update the docker-compose.yaml file to accept."
 fi
--- README.md
+++ README.md
@@ -4,7 +4,18 @@
 This repository contains the necessary files to build a Docker container for launching a Minecraft server with the Create: Astral modpack, using GraalVM Java 17.
 
 ## Instructions
-WIP
+*This container assumes that you possess at least some basic knowledge of navigating the linux commandline. I will link some resources further down for command basics and docker basics.*
+
+### Initial setup
+1. Clone the repository to an empty folder of your choice.
+2. Copy the examples from the template folder to the root of the folder (removing the .example from the name) or use your own if you prepared them or are re-installing
+3. Update the templates as appropriate for your server, keeping in mind the following:
+- You need to provide a password for rcon in the .env file. Also change default ram allocation for the server as needed.
+- `server.properties` only needs you to change options which are differnt to defaults (see https://server.properties for defaults), I provided the rcon related ones as you need to put a password that matches your .env file there
+- `ops.json` and `whitelist.json` are optional, make sure to actually change both the names and uuids. Feeel free to add more or less players there
+- The templates are separated so that no one accidentally commits/PRs their actual secrets, keep the examples in templates folder as is
+4. Open rcon.yaml and put a matching password from your .env file. If you edited the rcon port, make sure to change it here too.
+
 
 ## Disclaimer
 I do not own any of the software or mods contained in this repository. All rights and ownership belong to their respective creators, as listed below.
