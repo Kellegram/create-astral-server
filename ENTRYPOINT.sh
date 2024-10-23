95f268e069eb19b4feb1bc6343cd9537a499b8c3
Kellegram <kellegram@protonmail.ch>
2024-10-23 13:48:58 +0100

    Improve consistency in ENTRYPOINT

--- ENTRYPOINT.sh
+++ ENTRYPOINT.sh
@@ -3,13 +3,13 @@ if [ "$EULA" = "TRUE" ]; then
 	echo "eula=true" > eula.txt
 
 	rm -f server.properties
-	[ -f "/init/server.properties" ] && cp /init/server.properties server.properties
+	[ -f "/init/server.properties" ] && cp /init/server.properties server.properties || echo "Warning: /init/server.properties not found."
 
-	[ -f "/init/rcon.yaml" ] && cp /init/rcon.yaml /etc/rcon.yaml
+	[ -f "/init/rcon.yaml" ] && cp /init/rcon.yaml /etc/rcon.yaml || echo "Warning: /init/rcon.yaml not found."
 
 	if [ -n "$RCON_PASSWORD" ]; then
 		sed -i "s/^password:.*/password: $RCON_PASSWORD/" /etc/rcon.yaml
-		sed -i "s/^rcon.password=.*/rcon.password=$RCON_PASSWORD/" server.properties
+		sed -i "s/^rcon\.password=.*/rcon.password=$RCON_PASSWORD/" server.properties
 	fi
 
 	grep "^sync-chunk-writes" server.properties || echo "sync-chunk-writes=false" >>server.properties
@@ -28,7 +28,7 @@ if [ "$EULA" = "TRUE" ]; then
 	fi
 
 	if [ -n "$ALLOCATED_RAM" ]; then
-		exec java -Xms"$ALLOCATED_RAM"G -Xmx"$ALLOCATED_RAM"G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=20 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar server.jar --nogui
+		exec java -Xms"${ALLOCATED_RAM}G" -Xmx"${ALLOCATED_RAM}G" -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=20 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar server.jar --nogui
 	else
 		echo "Incorrect value for ALLOCATED_RAM provided or .env file missing. Please consult the README instructions if you haven't yet."
 	fi
